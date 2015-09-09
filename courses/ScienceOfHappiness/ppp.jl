70990259de2f1acbfa6b0e38af105372c659fb7a
Marcus Appelros <marcus.appelros@gmail.com>
2015-09-09 18:35:01 +0200

    <3

--- courses/ScienceOfHappiness/ppp.jl
+++ courses/ScienceOfHappiness/ppp.jl
@@ -5,14 +5,39 @@ files=["index.txt","week1.txt"]
 file=files[1]
 title="Science of Happiness"
 text=readall(file)
-while contains(text,"\n\n")
-	text=replace(text,"\n\n","<br>\n")
-end
 
+function skipto(ext,char)
+	for ci in 1:length(ext)
+		if ext[ci]==char
+			return ci
+		end
+	end
+	return 0
+end
 linkloc=search(text,"http")
-while linkloc!=0:-1
-
+while !isempty(collect(linkloc))
+	println(linkloc)
+	linkend=skipto(text[linkloc[end]:end],' ')
+	lineend=skipto(text[linkloc[end]:end],'\n')
+	linkend=min(linkend,lineend)
+	linkloc=linkloc[1]:linkloc[end]+linkend-2
+	link=text[linkloc]
+	if text[linkloc[1]-1]=='\n' && lineend>linkend
+		linktextloc=linkloc[end]+1:linkloc[4]+lineend-2
+		linktext=text[linktextloc]
+		htmltext="""<a href="$link">$linktext</a>"""
+		text=text[1:linkloc[1]-1]*htmltext*text[linktextloc[end]+1:end]
+	else
+		htmltext="""<a href="$link">$link</a>"""
+		text=text[1:linkloc[1]-1]*htmltext*text[linkloc[end]+1:end]
+	end
+	linkloc=search(text[linkloc[end]:end],"http")+linkloc[end]
+	println(linkloc)
 end
+
+#while contains(text,"\n\n")
+#	text=replace(text,"\n\n","<br>\n")
+#end
 
 doc="""<!DOCTYPE html>
 <html>
