8cab1e87fa8aa0b21306fa32c12c39c5a20d56f7
Marcus Appelros <marcus.appelros@gmail.com>
2015-09-09 17:45:26 +0200

    <3

--- courses/ScienceOfHappiness/ppp.jl
+++ courses/ScienceOfHappiness/ppp.jl
@@ -1,3 +1,27 @@
 #Preprintprocessor
 
 files=["index.txt","week1.txt"]
+
+file=files[1]
+title="Science of Happiness"
+text=readall(file)
+while contains(text,"\n\n")
+	text=replace(text,"\n\n","<br>\n")
+end
+
+linkloc=search(text,"http")
+while linkloc!=0:-1
+
+end
+
+doc="""<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>$title</title>
+</head>
+<body>
+
+
+</body>
+</html>"""
