f3835bfe07c4402183206727d920e377b8fb6fa7
Marcus Appelros <marcus.appelros@gmail.com>
2015-09-10 15:14:21 +0200

    <3

--- courses/ScienceOfHappiness/ppp.jl
+++ courses/ScienceOfHappiness/ppp.jl
@@ -1,52 +1,94 @@
 #Preprintprocessor
-
-files=["index.txt","week1.txt"]
-
-file=files[1]
-title="Science of Happiness"
-text=readall(file)
-
-function skipto(ext,char)
-	for ci in 1:length(ext)
-		if ext[ci]==char
+function skipto(str,char)
+	for ci in 1:length(str)
+		#print(str[ci])
+		if str[ci]==char
 			return ci
 		end
 	end
 	return 0
 end
-linkloc=search(text,"http")
-while !isempty(collect(linkloc))
-	println(linkloc)
-	linkend=skipto(text[linkloc[end]:end],' ')
-	lineend=skipto(text[linkloc[end]:end],'\n')
-	linkend=min(linkend,lineend)
-	linkloc=linkloc[1]:linkloc[end]+linkend-2
-	link=text[linkloc]
-	if text[linkloc[1]-1]=='\n' && lineend>linkend
-		linktextloc=linkloc[end]+1:linkloc[4]+lineend-2
-		linktext=text[linktextloc]
-		htmltext="""<a href="$link">$linktext</a>"""
-		text=text[1:linkloc[1]-1]*htmltext*text[linktextloc[end]+1:end]
-	else
-		htmltext="""<a href="$link">$link</a>"""
-		text=text[1:linkloc[1]-1]*htmltext*text[linkloc[end]+1:end]
+files=["index.txt","week1.txt"]
+
+function process(fname,title)
+	text=readall("$fname.txt")
+	linkloc=search(text,"http")
+	while !isempty(collect(linkloc))
+#		println(text[linkloc])
+		linkend=skipto(text[linkloc[end]:end],' ')
+		lineend=skipto(text[linkloc[end]:end],'\n')
+		linkend=min(linkend,lineend)
+		linkloc=linkloc[1]:linkloc[end]+linkend-2
+		link=text[linkloc]
+		if text[linkloc[1]-1]=='\n' && lineend>linkend
+			linktextloc=linkloc[end]+1:linkloc[4]+lineend-2
+			linktext=text[linktextloc]
+			htmltext="""<a href="$link">$linktext</a>"""
+			l=length(htmltext)
+			text=text[1:linkloc[1]-1]*htmltext*text[linktextloc[end]+1:end]
+		else
+			htmltext="""<a href="$link">$link</a>"""
+			l=length(htmltext)
+			text=text[1:linkloc[1]-1]*htmltext*text[linkloc[end]+1:end]
+		end
+		linkloc=search(text[linkloc[1]+l:end],"http")+linkloc[1]+l-1
+		#println(text)
 	end
-	linkloc=search(text[linkloc[end]:end],"http")+linkloc[end]
-	println(linkloc)
+	hloc=search(text,"*")
+	while !isempty(collect(hloc))
+		hn=text[hloc[1]+1]=='*'?(text[hloc[1]+2]=='*'?1:2):3
+		hstart=hloc[1]+4-hn
+		lineend=skipto(text[hstart:end],'\n')
+		htext=text[hstart:hstart+lineend-2]
+		htmltext="<h$hn>$htext</h$hn>"
+		text=text[1:hloc[1]-1]*htmltext*text[hstart+lineend-1:end]
+		hloc=search(text,"*")
+	end
+	ulloc=search(text,"<ul>")
+	while !isempty(collect(ulloc))
+		tulloc=search(text[ulloc[end]:end],"</ul>")+ulloc[end]-1
+		list=text[ulloc[end]+2:tulloc[1]-2]
+		listarray=readdlm(IOBuffer(list),'\n')
+		htmltext=""
+		for li in listarray
+			htmltext*="<li>$li</li>\n"
+		end
+		text=text[1:ulloc[end]]*htmltext*text[tulloc[1]:end]
+		ulloc=search(text[tulloc[end]:end],"<ul>")+tulloc[end]-1
+		#println(text[ulloc])
+	end
+	while contains(text,"\n\n")
+		text=replace(text,"\n\n","<br>\n")
+	end
+	while contains(text,"<easy>")
+	text=replace(text,"<easy>","""<span class="easy">""")
+	end
+	while contains(text,"<yellow>")
+		text=replace(text,"<yellow>","""<span class="medium">""")
+	end
+	while contains(text,"<red>")
+		text=replace(text,"<red>","""<span class="hard">""")
+	end
+
+	doc="""<!DOCTYPE html>
+	<html>
+	<head>
+	  <meta charset="utf-8">
+	  <title>$title</title>
+	<link rel="stylesheet" type="text/css" href="../style.css">
+	</head>
+	<body>
+
+	$text
+
+	</body>
+	</html>"""
+
+	f=open("$fname.html","w")
+	write(f,doc)
+	close(f)
+#	print(text)
 end
 
-#while contains(text,"\n\n")
-#	text=replace(text,"\n\n","<br>\n")
-#end
-
-doc="""<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <title>$title</title>
-</head>
-<body>
-
-
-</body>
-</html>"""
+process("index","Science of Happiness")
+process("week1","Week 1")
