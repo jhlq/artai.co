4a85f54ad497f391ecf6def342ec2cd8b77e7b8a
qni <marcus.appelros@gmail.com>
2015-12-20 01:47:48 +0100

    hexago

--- public_html/hexago.php
+++ public_html/hexago.php
@@ -19,9 +19,50 @@
 	<br>
 	<canvas id="HexCanvas" width="700" height="900"></canvas>
 	<br>
-	Tiles: <input type="number" id="tiles" value="19">
-	Tilesize: <input type="number" id="tilesize" value="21">
-	Colors: <input type="string" id="colors" value="255,0,0;0,255,0;0,0,255;0,0,0;0,150,150;150,150,0;">
+	Tiles: <input type="number" id="tiles" value=<?php
+	$v=$_GET["tiles"];
+	if ($v==""){
+		echo "19";
+	}else{
+		echo $v;
+	}?>>
+	Tilesize: <input type="number" id="tilesize" value=<?php
+	$v=$_GET["tilesize"];
+	if ($v==""){
+		echo "21";
+	}else{
+		echo $v;
+	}?>>
+	Colors: <input type="string" id="colors" value=<?php
+	$v=$_GET["colors"];
+	if ($v==""){
+		echo "255,0,0;0,255,0;0,0,255;0,0,0;150,0,150;255,255,0;";
+	}else{
+		echo $v;
+	}?>>
+	<br>
+	Border.<br>
+	Start: <input type="number" id="borderstart" value=<?php
+	$v=$_GET["borderstart"];
+	if ($v==""){
+		echo "5";
+	}else{
+		echo $v;
+	}?>>
+	Length: <input type="number" id="borderlength" value=<?php
+	$v=$_GET["borderlength"];
+	if ($v==""){
+		echo "10";
+	}else{
+		echo $v;
+	}?>>
+	Color: <input type="string" id="bordercolor" value=<?php
+	$v=$_GET["bordercolor"];
+	if ($v==""){
+		echo "51,0,100";
+	}else{
+		echo $v;
+	}?>>
 	<br>
 	<button onclick="refresh()">Refresh</button>	
 	
@@ -29,13 +70,20 @@
     <script>
 	var nt = document.getElementById("tiles").value;
 	var ts = document.getElementById("tilesize").value;
-	var hexagonGrid = new HexagonGrid("HexCanvas", ts);
+	hexagonGrid = new HexagonGrid("HexCanvas", ts);
         hexagonGrid.drawHexGrid(nt, nt, 5, 5, true);
-	var border="3:9,0;3:10,1;3:11,1;3:12,2;3:13,2;3:14,3;3:15,3;3:16,4;3:17,4;3:18,5;3:18,6;3:18,7;3:18,8;3:18,9;3:18,10;3:18,11;3:18,12;3:18,13;3:18,14;3:17,14;3:16,15;3:15,15;3:14,16;3:13,16;3:12,17;3:11,17;3:10,18;3:9,18;3:8,18;3:7,17;3:6,17;3:5,16;3:4,16;3:3,15;3:2,15;3:1,14;3:0,14;3:0,13;3:0,12;3:0,11;3:0,10;3:0,9;3:0,8;3:0,7;3:0,6;3:0,5;3:1,4;3:2,4;3:3,3;3:4,3;3:5,2;3:6,2;3:7,1;3:8,1;".split(";");
-	for (i = 0; i < border.length-1; i++) {
-		var p=border[i].split(":");
-		var q=p[1].split(",");
-		hexagonGrid.drawHexAtColRow(q[0],q[1],"rgba(70,50,90,0.7)");
+	drawborder(parseInt(document.getElementById("borderstart").value),parseInt(document.getElementById("borderlength").value),document.getElementById("bordercolor").value);
+	function drawborder(start,length,color){
+		for (i=0;i<length;i++){
+			hexagonGrid.drawHexAtColRow(0,start+i,"rgba("+color+",0.9)");
+			hexagonGrid.drawHexAtColRow(length*2-2,start+i,"rgba("+color+",0.9)");
+		}
+		for (i=1;i<length;i++){
+			hexagonGrid.drawHexAtColRow(i,start-1-parseInt(i/2-0.5),"rgba("+color+",0.9)");
+			hexagonGrid.drawHexAtColRow(length-1+i,start-length/2+parseInt(i/2+0.5),"rgba("+color+",0.9)");
+			hexagonGrid.drawHexAtColRow(length+i-1,start-length/2+2*length-2-parseInt(i/2),"rgba("+color+",0.9)");
+			hexagonGrid.drawHexAtColRow(i,start+length+parseInt(i/2)-1,"rgba("+color+",0.9)");
+		}
 	}
 	function refresh(){
 		var canvas = document.getElementById('HexCanvas');
@@ -43,14 +91,14 @@
 		context.clearRect(0, 0, canvas.width, canvas.height);
 		var nt = document.getElementById("tiles").value;
 		var ts = document.getElementById("tilesize").value;
-		var hexagonGrid = new HexagonGrid("HexCanvas", ts);
+		hexagonGrid = new HexagonGrid("HexCanvas", ts);
 		hexagonGrid.drawHexGrid(nt, nt, 5, 5, true);
+		drawborder(parseInt(document.getElementById("borderstart").value),parseInt(document.getElementById("borderlength").value),document.getElementById("bordercolor").value);
+		playplays();
 	}
 	function playplays(){
-		//var plays=(document.getElementById("plays").value);
 		playsequence(document.getElementById("plays").value);
 	}
-	//playplays();
 	function playsequence(seq){
 		plays=seq.split(";");
 		var c=(document.getElementById("colors").value).split(";");
