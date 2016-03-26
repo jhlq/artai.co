bc9f30ad998dc9aa14047ec7ae9aecc3f4cf92fe
qni <marcus.appelros@gmail.com>
2016-03-26 16:52:52 +0100

    createmap

---
+++ public_html/createmap.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<head>
+	<title>Hexago</title>
+	<script src="hexagon.js"></script>
+</head>
+<body>
+
+<?php
+	$map=$_POST["map"];
+	$plays=$_POST["plays"];
+	$player=$_POST["player"];
+	echo $map;
+	try
+	{
+		$db = new PDO('sqlite:data/mapstest.sqlite');
+
+		$s="CREATE TABLE ".$map." (Id INTEGER PRIMARY KEY, plays TEXT, player INTEGER)";
+		$db->exec($s);	 
+		$s="INSERT INTO ".$map." (plays,player) VALUES ('".$plays."',".$player.");";
+		$db->exec($s);
+
+		print "<table border=1>";
+		print "<tr><td>Id</td><td>plays</td><td>player</td></tr>";
+		$result = $db->query('SELECT * FROM '.$map);
+		foreach($result as $row){
+			print "<tr><td>".$row['Id']."</td>";
+			print "<td>".$row['plays']."</td>";
+			print "<td>".$row['player']."</td></tr>";
+		}
+		print "</table>";
+		
+		$result = $db->query('SELECT * FROM '.$map);
+		if(empty($result))
+		{
+			print "This map is empty.";
+		} else {
+			$r=$result->fetch(PDO::FETCH_BOTH, PDO::FETCH_ORI_LAST);
+			print "This map contains the following plays: ";
+			print $r['plays'];
+			print '<br>';
+			#print "This map contains the following plays: ".$result[0]['plays'];
+			#print "The current player is: ".$result[0]['player'];
+			$link='http://artai.co/hexago.php?plays='.$r['plays'].'&player='.$r['player'];
+			print 'To view this map go to the following link: <a href="'.$link.'">'.$link.'</a> ';
+		}
+
+
+		$db = NULL;
+	}
+	catch(PDOException $e)
+	{
+		print 'Exception : '.$e->getMessage();
+	}
+?>
+
+</body>
+</html>
