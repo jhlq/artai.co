96addc8c8cdf94ff004d555227dc73d0690c0c8f
Marcus Appelros <marcus.appelros@gmail.com>
2016-05-27 03:19:08 +0200

    tournaments

--- hexago/hexago.jl
+++ hexago/hexago.jl
@@ -43,7 +43,13 @@ function makemap(radius=6)
 	end
 	return map		
 end
-function sprinf!(map,x,y,c,ir) #spread influence
+function resetinf!(map)
+	for (k,v) in map.locs
+		map.locs[k].inf=Dict{Int,AbstractFloat}(1=>0,2=>0,3=>0)
+	end
+	return map
+end
+function sprinf!(map,x,y,c,ir=6) #spread influence
 	visited=[(x,y)]
 	fringe=[(x,y)]
 	for i in 1:ir
@@ -56,6 +62,7 @@ function sprinf!(map,x,y,c,ir) #spread influence
 					ladj=(adj.x,adj.y)
 					if !in(ladj,visited) && haskey(map.locs,ladj)
 						map.locs[ladj].inf[c]+=1.0/i
+#if (x,y)==(0,-1) && i==2;println(map.locs[ladj],' ',c);end
 						if map.locs[ladj].col==0 || map.locs[ladj].col==c 
 							push!(nfringe,ladj)
 						end
@@ -63,6 +70,7 @@ function sprinf!(map,x,y,c,ir) #spread influence
 					end 
 				end
 			end
+#if (x,y)==(0,-1) && i==2;println(nfringe);end
 			if length(nfringe)==0
 				break
 			end
@@ -71,7 +79,14 @@ function sprinf!(map,x,y,c,ir) #spread influence
 	end
 	return map
 end
-function place!(map,x,y,c,ir=6)
+function sprinfall!(map,ir=6)
+	resetinf!(map)
+	for mov in map.moves
+		sprinf!(map,mov[1][1],mov[1][2],mov[2],ir)
+	end
+	return map
+end
+function place!(map,x,y,c)
 	push!(map.moves,[(x,y),c])
 	if x==y==0
 		return "pass"
@@ -81,12 +96,13 @@ function place!(map,x,y,c,ir=6)
 	end
 	map.locs[x,y].hex=HC(x,y)	
 	map.locs[x,y].col=c
-	sprinf!(map,x,y,c,ir)
+	#sprinf!(map,x,y,c,ir)
+	return map
 end
 #place!(map,xy,c)=place!(map,xy[1],xy[2],c)
-function place!(map,xysc::Array,ir=6)
+function place!(map,xysc::Array)
 	for xyc in xysc
-		place!(map,xyc[1][1],xyc[1][2],xyc[2],ir)
+		place!(map,xyc[1][1],xyc[1][2],xyc[2])
 	end
 	return map
 end
@@ -122,7 +138,7 @@ function max(d::Dict)
 	k=Int[]
 	m=-Inf
 	for (key,val) in d
-		if val==m
+		if val==m && val!=0
 			push!(k,key)
 		elseif val>=m
 			m=val
@@ -134,7 +150,7 @@ end
 function points(l::Loc)
 	maxinf=max(l.inf)
 	p=1
-	if !in(l.col,maxinf)
+	if l.col!=0 && !in(l.col,maxinf)
 		p+=1
 	end
 	c=Dict{Int,AbstractFloat}()
@@ -143,17 +159,30 @@ function points(l::Loc)
 	end
 	return c
 end
-function score(map::Map)
+function countmoves(map,col)
+	n=0
+	for m in map.moves
+		if m[2]==col
+			n+=1
+		end
+	end
+	return n
+end
+function score(map::Map,ir=6)
+	sprinfall!(map,ir)
 	s=Dict{Int,AbstractFloat}(1=>0,2=>0,3=>0)
 	for (key,val) in map.locs
 		for (c,p) in points(val)
 			s[c]+=p
 		end
 	end
+	for (key,val) in s
+		s[key]-=countmoves(map,key)
+	end
 	return s
 end
 
-function relinf(inf::Dict,col) #relative influense
+function relinf(inf::Dict,col) #relative influence
 	cinf=inf[col]
 	minf=0
 	for (k,v) in inf
@@ -167,9 +196,10 @@ type AI
 	net
 	bias
 	col::Int
+	wins
 end
-AI(col::Int)=AI(rand(3,3)-0.5,rand(3)-0.5,col)
-AI()=AI(1)
+AI(nn=5,col::Int=1)=AI(rand(nn,nn)-0.5,rand(nn)-0.5,col,0)
+#AI()=AI(1)
 ask(ai::AI,data::Number)=ask(ai::AI,[data])
 function ask(ai::AI,data::Array)
 	nn=size(ai.net)[1]
@@ -193,10 +223,10 @@ function sanity(ai::AI)
 	end
 	false
 end
-function makesaneAI()
-	ai=AI()
+function makesaneAI(nn=5,col=1)
+	ai=AI(nn,col)
 	while !sanity(ai)
-		ai=AI()
+		ai=AI(nn,col)
 	end
 	ai
 end
@@ -204,7 +234,7 @@ function letAIplay(map::Map,ai::AI)
 #opening evaluate whole board -> decide on segment
 	cornerscores=Array[]
 	move=(0,0)
-	bestscore=-1000
+	likest=-1000 #most liked
 	for corneri in 1:6
 		cr=round(Int,map.r/2)
 		cornercenter=neighbor(HC(0,0),corneri,cr)
@@ -212,17 +242,36 @@ function letAIplay(map::Map,ai::AI)
 		if l.col!=0
 			continue
 		end
+		sprinfall!(map)
 		data=relinf(l.inf,ai.col)
+#println(data)
 		r=ask(ai,data)
 		push!(cornerscores,r)
-		if sum(r)>bestscore
-			bestscore=sum(r)
+		if sum(r)>likest
+			likest=sum(r)
 			move=(cornercenter.x,cornercenter.y)
 		end
 #		for hex in ring(cornercenter,cr)
 #			l=map[hex.x,hex.y]
 #			data=relinf(l.inf,ai.col)
 #		end
+	end
+	for mov in map.moves
+		if mov[2]==ai.col
+			for hex in ring(HC(mov[1]...),1)
+				if haskey(map.locs,(hex.x,hex.y))
+					l=map.locs[hex.x,hex.y]
+					if l.col==0
+						data=relinf(l.inf,ai.col)
+						r=ask(ai,data)
+						if sum(r)>likest
+							likest=sum(r)
+							move=(hex.x,hex.y)
+						end
+					end
+				end
+			end
+		end
 	end
 	place!(map,move[1],move[2],ai.col)
 	return move,cornerscores
@@ -234,4 +283,52 @@ function letAIplay(map::Map,ai::AI)
 	#up or down?
 #inspect connections
 
+end
+
+function match(ai1::AI,ai2::AI,nmoves::Int)
+	map=makemap()
+	ai1.col=1
+	ai2.col=2
+	for nm in 1:nmoves
+		letAIplay(map,ai1)
+		letAIplay(map,ai2)
+	end
+	return map
+end
+function makeAIs(nais::Int)
+	ais=AI[]
+	for n in 1:nais
+		push!(ais,makesaneAI())
+	end
+	return ais
+end
+function tournament!(ais,nmoves::Int)
+	for ai1 in ais
+		for ai2 in ais
+			if ai1!=ai2
+				m=match(ai1,ai2,nmoves)
+				s=score(m)
+				#println(s[1]," ",s[2]," ",max(s)," ",to_s(m))
+				for c in max(s)
+					[ai1,ai2][c].wins+=1
+				end
+			end
+		end
+	end
+	return ais
+end
+import Base.isless
+isless(ai1::AI,ai2::AI)=isless(ai1.wins,ai2.wins)
+function winners(ais)
+	ws=AI[]
+	mw=0
+	for ai in ais
+		if ai.wins>mw
+			ws=[ai]
+			mw=ai.wins
+		elseif ai.wins==mw
+			push!(ws,ai)
+		end
+	end
+	return ws
 end
